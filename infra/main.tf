5de11423c76cfde7b8c628dcf8e7899d2f4f1808
shravanj09 <shravan.j09@gmail.com>
2026-02-08 14:07:13 -0500

    Fix storage account arg for provider

--- infra/main.tf
+++ infra/main.tf
@@ -21,7 +21,6 @@ resource "azurerm_storage_account" "sa" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   min_tls_version          = "TLS1_2"
-  allow_blob_public_access = false
   tags                     = var.tags
 }
 
